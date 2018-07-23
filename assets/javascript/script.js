d7f979d92305a72dd0b561592449e8d6c194261d
matteichler <matteichler7@gmail.com>
2018-07-23 13:40:23 -0700

    added styling

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+var config = {
+  apiKey: "AIzaSyBlnZE6fXHwyGWPKXsjUPDNFcrQnewwQlU",
+  authDomain: "mattre7firstproject.firebaseapp.com",
+  databaseURL: "https://mattre7firstproject.firebaseio.com",
+  projectId: "mattre7firstproject",
+  storageBucket: "mattre7firstproject.appspot.com",
+  messagingSenderId: "345482362969"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
