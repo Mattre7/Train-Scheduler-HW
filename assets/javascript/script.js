f83604cf6002f5efcbba8a078f032cbe8ff4bde2
matteichler <matteichler7@gmail.com>
2018-07-25 16:30:23 -0700

    added trains, need to fix moment.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,46 +4,71 @@ var config = {
   databaseURL: "https://matts-train-scheduler.firebaseio.com",
   projectId: "matts-train-scheduler",
   storageBucket: "matts-train-scheduler.appspot.com",
-  messagingSenderId: "345482362969"
+  messagingSenderId: "667900520974"
 };
 
 firebase.initializeApp(config);
 var database = firebase.database();
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function(childSnapshot) {
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var trainTime = childSnapshot.val().trainTime;
     var frequency = childSnapshot.val().frequency;
-    console.log(trainName);
-    console.log(destination);
+
+    console.log(moment(trainTime).isValid());
     console.log(trainTime);
+    console.log(moment.unix(trainTime));
+
+    var timeDiff = moment().diff(moment.unix(trainTime), "minutes");
+    var timeRemainder = moment().diff(moment.unix(trainTime), "minutes") % frequency;
+    var minutes = frequency - timeRemainder;
+    var nexttrain = moment().add(minutes, "m").format("hh:mm A");
+
+    console.log(timeDiff);
     console.log(frequency);
+    console.log(timeRemainder);
+
+    console.log(minutes)
+    console.log(nexttrain)
+    console.log(moment().format('hh:mm A'));
+    console.log(nexttrain);
+    console.log(moment().format('X'))
+
+    var newTraindiv = $("<div class='row newtraindiv'>")
+    $(".train-group").append(newTraindiv)
+    newTraindiv.append($("<h6 class='col-sm-3'>").text(trainName))
+    newTraindiv.append($("<h6 class='col-sm-3'>").text(destination))
+    newTraindiv.append($("<h6 class='col-sm-2'>").text(frequency))
+    newTraindiv.append($("<h6 class='col-sm-2'>").text(nexttrain))
+    newTraindiv.append($("<h6 class='col-sm-2'>").text(minutes))
 })
 
 $(".addtrain").on("click", function(){
+    event.preventDefault();
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var trainTime = $("#trainTime").val().trim();
-    var nextArrival = moment($("#trainTime").val().trim(), "HH:mm").subtract(10, "years").format("X");;
+    var trainTime = $("#trainTime").val();
+//    var nextArrival = moment($("#trainTime").val(), "HH:mm").subtract(10, "years").format("X");;
     var frequency = $("#frequency").val().trim();
     console.log(trainName);
     console.log(destination);
     console.log(trainTime);
-    console.log(nextArrival)
+//    console.log(nextArrival)
     console.log(frequency);
     var newTrain = {
         trainName: trainName,
         destination: destination,
         trainTime: trainTime,
-        nextArrival: nextArrival,
+//        nextArrival: nextArrival,
         frequency: frequency,
     };
     database.ref().push(newTrain)
+
     console.log(newTrain)
     $("#trainName").val("");
     $("#destination").val("");
     $("#trainTime").val("");
     $("#frequency").val("");
-    return false;
+    // return false;
 });
